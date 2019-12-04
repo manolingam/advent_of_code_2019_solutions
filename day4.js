cd6d2f88dc6e5a0470127c4010f11eb55470ab02
Manolingam <manolingam1996@gmail.com>
2019-12-04 13:26:33 +0530

    added solution to day 4 - part 1

---
+++ day4.js
@@ -0,0 +1,35 @@
+//Day 4 - Part 1
+
+let count = 0
+
+for(let i = 307237; i <= 769058; i++) {
+
+  let num = i
+  num = num.toString()
+  let characters = Array.from(num)
+  let max = characters[0]
+  let maxIndex = 0
+  let check = false
+
+  for(let j = 0; j < characters.length; j++) {
+    if(characters[j] < max) {
+      check = false
+      break
+    } else if(characters[j] == max && j == maxIndex + 1) {
+      check = true
+      max = characters[j]
+      maxIndex = j
+    } else {
+      max = characters[j]
+      maxIndex = j
+    }
+  }
+
+  if(check) {
+    count++
+    console.log(i)
+  }
+
+}
+
+console.log(count)
